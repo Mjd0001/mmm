69970056c072647fdf53993691db25cf17879fca
Majd Ahmad <105239889+Mjd0001@users.noreply.github.com>
2025-10-23 19:13:34 +0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+model = pickle.load(open('trained_model.sav', 'rb'))
+
+st.title('iris Prediction Project')
+
+sepal_l = st.number_input('Sepal Length')
+sepal_w = st.number_input('Sepal Width')
+petal_l = st.number_input('Petal Length')
+petal_w = st.number_input('Petal Width')
+
+input_data = np.asarray([[sepal_l, sepal_w, petal_l , petal_w]])
+
+result = ''
+
+if st.button('Result'):
+    result = model.predict(input_data)[0]
+
+
+st.success(result)
